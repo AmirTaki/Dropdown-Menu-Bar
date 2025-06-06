d0be854e35118963a44497f15cae5d5d2591bc56
amir <amirtaki623@gmail.com>
2025-06-06 22:34:57 +0330

    add js part 2 at project

--- src/main.js
+++ src/main.js
@@ -2,4 +2,14 @@
 const showMenu = (toggleId, navId)=>{
     const toggle = document.getElementById(toggleId),
         nav = document.getElementById(navId);
-}
+
+        toggle.addEventListener("click", ()=>{
+            // show menu class to nav menu
+            nav.classList.toggle("show-menu")
+            // add show icon to show and hide menu icon
+            toggle.classList.toggle('show-icon')
+        })
+
+ }
+
+ showMenu('nav-toggle', 'nav-menu');
